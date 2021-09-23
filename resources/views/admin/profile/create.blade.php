6345fbb55e4071afb1b7876ea3ede4672a8c5334
nakahama <68jmllnmpt7w@gmail.com>
2021-09-23 07:41:04 +0000

    課題11

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -13,3 +13,21 @@ initial-scale=1">
         <h1>Myプロフィール作成画面(create)</h1>
     </body>
 </html>
+
+{{-- layouts/admin.blade.phpを読み込む --}}
+@extends('layouts.admin')
+
+
+{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+@section('title', 'プロフィールの新規作成')
+
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール新規作成</h2>
+            </div>
+        </div>
+    </div>
+@endsection
