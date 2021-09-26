9b012a3eb11ccc603a41a25cdd640296eeac4c9a
nakahama <68jmllnmpt7w@gmail.com>
2021-09-26 05:57:09 +0000

    11課題修正

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -15,7 +15,7 @@ initial-scale=1">
 </html>
 
 {{-- layouts/admin.blade.phpを読み込む --}}
-@extends('layouts.admin')
+@extends('layouts.profile')
 
 
 {{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
